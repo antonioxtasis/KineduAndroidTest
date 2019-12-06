0d1d04d1312c9437ae7c974cd735dac4230419ca
Antonio Cordova <antonioxtasis.cr@gmail.com>
2019-12-05 21:42:11 -0600

    MainActivity stuff

--- app/src/main/java/com/antoniocordova/kineduandroidtest/ui/main/MainActivity.java
+++ app/src/main/java/com/antoniocordova/kineduandroidtest/ui/main/MainActivity.java
@@ -4,9 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
 
 import com.antoniocordova.kineduandroidtest.R;
+import com.blogspot.atifsoftwares.animatoolib.Animatoo;
 import com.google.android.material.tabs.TabLayout;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +31,33 @@ public class MainActivity extends AppCompatActivity {
 
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
+
+        ImageView imgBack = findViewById(R.id.img_back);
+        imgBack.setOnClickListener(v -> onBackPressed());
+
+        Spinner spinnerFilterByAge = findViewById(R.id.spinner_filter_by_age);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.array_filter_age, R.layout.spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerFilterByAge.setAdapter(adapter);
+        spinnerFilterByAge.setOnItemSelectedListener(
+                new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                        //travel_type = String.valueOf(adapterView.getItemAtPosition(i));
+                        //Toast.makeText(Plan_Trip.this, travel_type, Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> adapterView) { }
+                }
+        );
     }
 
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Animatoo.animateFade(this);
+    }
 }
